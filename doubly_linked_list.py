57676bb9c8109728fa0096d003b152a73c884df3
Starlight Romero <starlightromero@gmail.com>
2020-10-29 13:00:05 -0700

    added delete_head and delete_tail methods

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -7,7 +7,7 @@ class DoublyLinkedList(LinkedList):
     """Doubly Linked List class."""
 
     def prepend(self, data):
-        """Prepend data to linked list."""
+        """Prepend data to doubly linked list."""
         new_head = DoublyNode(data, None, self.head)
         self.head = new_head
         if self.tail is None:
@@ -15,7 +15,7 @@ class DoublyLinkedList(LinkedList):
             self.tail.prev = self.head
 
     def append(self, data):
-        """Append data to linked list."""
+        """Append data to doubly linked list."""
         new_tail = DoublyNode(data)
         if self.tail:
             new_tail.prev = self.tail
@@ -27,17 +27,6 @@ class DoublyLinkedList(LinkedList):
         if self.head is None:
             self.head = new_tail
 
-    # def find(self, data):
-    #     """Find a node with the given data."""
-    #     if self.head is None:
-    #         return None
-    #     cur_node = self.head
-    #     while cur_node:
-    #         if cur_node.data == data:
-    #             return cur_node
-    #         cur_node = cur_node.next
-    #     return None
-    #
     # def delete(self, data):
     #     """Delete node with given data."""
     #     if self.head is None:
@@ -50,33 +39,33 @@ class DoublyLinkedList(LinkedList):
     #             cur_node.next = cur_node.next.next
     #         else:
     #             cur_node = cur_node.next
-    #
-    # def delete_head(self):
-    #     """Delete head of linked list."""
-    #     if self.head is None:
-    #         return None
-    #     deleted_node = self.head
-    #     if self.head.next:
-    #         self.head = self.head.next
-    #     else:
-    #         self.head = None
-    #         self.tail = None
-    #     return deleted_node
-    #
-    # def delete_tail(self):
-    #     """Delete tail of linked list."""
-    #     if self.tail is None:
-    #         return None
-    #     deleted_node = self.tail
-    #     cur_node = self.head
-    #     while cur_node.next:
-    #         if cur_node.next == self.tail:
-    #             cur_node.next = None
-    #             self.tail = cur_node
-    #         else:
-    #             cur_node = cur_node.next
-    #     return deleted_node
-    #
+
+    def delete_head(self):
+        """Delete head of doubly linked list."""
+        if self.head is None:
+            return None
+        deleted_node = self.head
+        if self.head.next:
+            self.head = self.head.next
+            self.head.prev = None
+        else:
+            self.head = None
+            self.tail = None
+        return deleted_node
+
+    def delete_tail(self):
+        """Delete tail of doubly linked list."""
+        if self.tail is None:
+            return None
+        deleted_node = self.tail
+        if self.tail.prev:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            self.head = None
+            self.tail = None
+        return deleted_node
+
     # def print_list(self):
     #     """Print linked list."""
     #     current = self.head
