b377fcd673122e7c77eab889a213f3cbbb092c3d
Starlight Romero <starlightromero@gmail.com>
2020-10-29 13:19:47 -0700

    added Song: and Next: for repr and str methods

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 """Import LinkedList class."""
 from linked_list import LinkedList
 
-data = ["1", "2", "3", "4", "5"]
+data = [
+    "The Payback",
+    "I Want To Take You Higher",
+    "Give Up The Funk",
+    "Jungle Boogie",
+    "Pick Up the Pieces",
+    "Kiss",
+]
 
 
 ll = LinkedList()
 
-for d in data:
-    ll.append(d)
+for song in data:
+    ll.append(song)
+
+ll.print_list()
+print("")
+ll.prepend("Superfly")
+ll.print_list()
+print("")
+print(ll.find("Jungle Boogie"))
+print("")
