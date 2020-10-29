485b9a2b78622d8ca0d33b4c6f1be4b64e39d195
Starlight Romero <starlightromero@gmail.com>
2020-10-29 12:56:23 -0700

    added prepend and append methods

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -5,3 +5,102 @@ from doubly_node import DoublyNode
 
 class DoublyLinkedList(LinkedList):
     """Doubly Linked List class."""
+
+    def prepend(self, data):
+        """Prepend data to linked list."""
+        new_head = DoublyNode(data, None, self.head)
+        self.head = new_head
+        if self.tail is None:
+            self.tail = new_head
+            self.tail.prev = self.head
+
+    def append(self, data):
+        """Append data to linked list."""
+        new_tail = DoublyNode(data)
+        if self.tail:
+            new_tail.prev = self.tail
+            self.tail.next = new_tail
+        elif self.head:
+            new_tail.prev = self.head
+            self.head.next = new_tail
+        self.tail = new_tail
+        if self.head is None:
+            self.head = new_tail
+
+    # def find(self, data):
+    #     """Find a node with the given data."""
+    #     if self.head is None:
+    #         return None
+    #     cur_node = self.head
+    #     while cur_node:
+    #         if cur_node.data == data:
+    #             return cur_node
+    #         cur_node = cur_node.next
+    #     return None
+    #
+    # def delete(self, data):
+    #     """Delete node with given data."""
+    #     if self.head is None:
+    #         return None
+    #     while self.head and self.head.data == data:
+    #         self.head = self.head.next
+    #     cur_node = self.head
+    #     while cur_node.next:
+    #         if cur_node.next.data == data:
+    #             cur_node.next = cur_node.next.next
+    #         else:
+    #             cur_node = cur_node.next
+    #
+    # def delete_head(self):
+    #     """Delete head of linked list."""
+    #     if self.head is None:
+    #         return None
+    #     deleted_node = self.head
+    #     if self.head.next:
+    #         self.head = self.head.next
+    #     else:
+    #         self.head = None
+    #         self.tail = None
+    #     return deleted_node
+    #
+    # def delete_tail(self):
+    #     """Delete tail of linked list."""
+    #     if self.tail is None:
+    #         return None
+    #     deleted_node = self.tail
+    #     cur_node = self.head
+    #     while cur_node.next:
+    #         if cur_node.next == self.tail:
+    #             cur_node.next = None
+    #             self.tail = cur_node
+    #         else:
+    #             cur_node = cur_node.next
+    #     return deleted_node
+    #
+    # def print_list(self):
+    #     """Print linked list."""
+    #     current = self.head
+    #     while current is not None:
+    #         print(current.data)
+    #         current = current.next
+    #
+    # def to_array(self):
+    #     """Return an array from the linked list."""
+    #     arr = []
+    #     cur_node = self.head
+    #     while cur_node:
+    #         arr.append(cur_node.data)
+    #         cur_node = cur_node.next
+    #     return arr
+    #
+    # def reverse(self):
+    #     """Reverse linked list."""
+    #     if self.head is None or self.tail is None:
+    #         return None
+    #     arr = self.to_array().reverse()
+    #     new_ll = LinkedList()
+    #     index = 0
+    #     while index < len(arr):
+    #         new_ll.append(arr[index])
+    #         index += 1
+    #     return new_ll
