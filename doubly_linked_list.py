cadde4f29166d8ca11da0bfc3046a6bbcbbc1208
Starlight Romero <starlightromero@gmail.com>
2020-10-29 11:47:44 -0700

    created DoublyLinkedList class which inherits from LinkedList

---
+++ doubly_linked_list.py
@@ -0,0 +1,7 @@
+"""Doubly Linked List which contains many nodes."""
+from linked_list import LinkedList
+from doubly_node import DoublyNode
+
+
+class DoublyLinkedList(LinkedList):
+    """Doubly Linked List class."""
