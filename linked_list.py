cb80e3ae2b91dd271c8da5bab51e69b40515020e
Starlight Romero <starlightromero@gmail.com>
2020-10-29 09:42:29 -0700

    added Linked List class

---
+++ linked_list.py
@@ -0,0 +1,10 @@
+"""Linked List which contains many nodes."""
+
+
+class LinkedList:
+    """Linked List class."""
+
+    def __init__(self, head=None, tail=None):
+        """Initialize class with head and tail."""
+        self.head = head
+        self.tail = tail
