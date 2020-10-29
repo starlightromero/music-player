2faad82b22a37299262ccf1b179fda4a21d71b48
Starlight Romero <starlightromero@gmail.com>
2020-10-29 13:43:23 -0700

    performed and printed all methods for LinkedList class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Import LinkedList class."""
 from linked_list import LinkedList
+from doubly_linked_list import DoublyLinkedList
 
 data = [
     "The Payback",
@@ -11,15 +12,60 @@ data = [
 ]
 
 
+#                                   LINKED LIST
+
+
+# Create linked list
 ll = LinkedList()
 
+# Append each song in data to linked list
 for song in data:
     ll.append(song)
 
+# Print linked list
+print("LINKED LIST")
 ll.print_list()
 print("")
+
+# Prepend "Superfly" and print linked list
+print("PREPEND SUPERFLY")
 ll.prepend("Superfly")
 ll.print_list()
 print("")
+
+# Find "Jungle Boogie" and print node
+print("FIND JUNGLE BOOGIE")
 print(ll.find("Jungle Boogie"))
 print("")
+
+# Delete "Jungle Boogie" and print linked list
+print("DELETE JUNGLE BOOGIE")
+ll.delete("Jungle Boogie")
+ll.print_list()
+print("")
+
+# Delete head and print linked list
+print("DELETE HEAD")
+ll.delete_head()
+ll.print_list()
+print("")
+
+# Delete tail and print linked list
+print("DELETE TAIL")
+ll.delete_tail()
+ll.print_list()
+print("")
+
+# Covert linked list to list
+print("TO ARRAY")
+print(ll.to_array())
+print("")
+
+# Reverse linked list
+print("REVERSE LINKED LIST")
+reverse_ll = ll.reverse()
+print(reverse_ll.print_list())
+print("")
+
+
+#                           DOUBLY LINKED LIST
